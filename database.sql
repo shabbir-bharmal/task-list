db02a01cd7645710b574dc5e5812eee90adb968f
Shabbir Bharmal <developers.shabbir@arsenaltech.com>
2024-09-05 16:45:55 +0530

    Added sample database

---
+++ database.sql
@@ -0,0 +1,4 @@
+insert into `tasks` (`created_at`, `id`, `task_name`, `task_status`) values ('2024-09-05 16:35:48', 101, 'meeting', 0);
+insert into `tasks` (`created_at`, `id`, `task_name`, `task_status`) values ('2024-09-05 16:35:59', 102, 'shoping', 0);
+insert into `tasks` (`created_at`, `id`, `task_name`, `task_status`) values ('2024-09-05 16:36:14', 103, 'medical checkup', 0);
+insert into `tasks` (`created_at`, `id`, `task_name`, `task_status`) values ('2024-09-05 16:36:19', 104, 'pay bills', 0);
